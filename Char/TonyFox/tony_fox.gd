6a448f81ae5522f0acdeeae2f94c9e3a8e8d3fb9
Kevin David Kerbl <kevin.kerbl@stud.fh-campuswien.ac.at>
2024-07-11 23:09:42 +0200

    added sword mechanics

--- Char/TonyFox/tony_fox.gd
+++ Char/TonyFox/tony_fox.gd
@@ -4,9 +4,14 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
 @onready var anim = $AnimationPlayer
 @onready var jumpAud = $JumpAud
 @onready var sprite = $AnimatedSprite2D
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
 
 var inventory = {
 	"sword": true,
@@ -15,63 +20,65 @@ var inventory = {
 }
 
 func jump():
-		jumpAud.play()
-		velocity.y = JUMP_VELOCITY
-		if (inventory["sword"]==true):
-			anim.play("Jump_s")
-		else:
-			anim.play("Jump")
-				
+	jumpAud.play()
+	velocity.y = JUMP_VELOCITY
+	if inventory["sword"]:
+		state_machine.travel("Jump_s")
+	else:
+		state_machine.travel("Jump")
+		
+func perform_sword_attack():
+	state_machine.travel("sword_attack")
 
 func _physics_process(delta):
-	set_collision_mask_value(9,true)	
+	set_collision_mask_value(9, true)	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 	if Input.is_action_pressed("move_down"):
-		set_collision_mask_value(9,false)
+		set_collision_mask_value(9, false)
 		
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction == -1:
 		sprite.flip_h = true
 	elif direction == 1:
 		sprite.flip_h = false
-	var sword=inventory["sword"]
-	if(inventory["sword"]==true):
+
+	if inventory["sword"]:
+		if Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("attack") or direction:
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				jump()
+			if Input.is_action_just_pressed("attack"):
+				
+				perform_sword_attack()
+			
+				
+			if direction:
+				velocity.x = direction * SPEED
+				if velocity.y == 0:
+					state_machine.travel("Run_s")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if velocity.y == 0:
+				state_machine.travel("Idle_s")
+			elif velocity.y > 0:
+				state_machine.travel("Fall_s")
+	else:
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			jump()
 		if direction:
 			velocity.x = direction * SPEED
 			if velocity.y == 0:
-				anim.play("Run_s")
+				state_machine.travel("Run")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			if velocity.y == 0:
-				anim.play("Idle_s")
+				state_machine.travel("Idle")
 			if velocity.y > 0:
-				anim.play("Fall_s")
-	else:		
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			jump()
-		if direction:
-			velocity.x = direction * SPEED
-			if velocity.y == 0:
-				anim.play("Run")
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			if velocity.y == 0:
-				anim.play("Idle")
-			if velocity.y > 0:
-				anim.play("Fall")
+				state_machine.travel("Fall")
 	
 	move_and_slide()
 	
-	if inventory["sword"] and Input.is_action_just_pressed("attack"):
-		perform_sword_attack()
-
-func perform_sword_attack():
-	anim.play("sword_attack")
-
 func acquire_item(item_name: String):
 	if item_name in inventory:
 		inventory[item_name] = true
