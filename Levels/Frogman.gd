36cb8bc093b275d68ea819ab8059d6327080bd5f
Kevin David Kerbl <kevin.kerbl@stud.fh-campuswien.ac.at>
2024-07-12 16:32:19 +0200

    added sword tutorial

--- Levels/Frogman.gd
+++ Levels/Frogman.gd
@@ -1,12 +1,18 @@
 extends Sprite2D
 
 var playername = "TonyFox"
+@onready var lbl1 = $Label
+@onready var lbl2 = $Label2
 
 func _process(delta):
 	if Game.killedfrog == true:
 		$".".queue_free()
+		
+		
 
 func _on_area_2d_body_entered(body):
 	if body.name == playername:
 		Game.inventory["sword"] = true
 		Game.inventory["boots"] = true
+		lbl2.show()
+		lbl1.hide()
