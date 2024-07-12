1a60216d31696f1f24007ec8f0790a506c980f13
Kevin David Kerbl <kevin.kerbl@stud.fh-campuswien.ac.at>
2024-07-12 10:31:48 +0200

    added coins

--- Collectables/Coin/coin.gd
+++ Collectables/Coin/coin.gd
@@ -2,7 +2,7 @@ extends Area2D
 @onready var gem = $"."
 func _on_body_entered(body):
 	if body.name == "TonyFox":
-	   
+		Game.coins += 1
 		var tween1 = gem.create_tween()
 		var tween2 = gem.create_tween()
 		tween1.tween_property(self,"position",position-Vector2(0,25),0.3)
