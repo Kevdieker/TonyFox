cfea9deac3d89472bd1454dadf17d4b44a52a770
Kevin David Kerbl <kevin.kerbl@stud.fh-campuswien.ac.at>
2024-07-12 11:15:06 +0200

    added Checkpoint awareness

--- Collectables/Checkpoint/checkpoint.gd
+++ Collectables/Checkpoint/checkpoint.gd
@@ -2,8 +2,11 @@ extends Area2D
 
 @onready var anim = $AnimatedSprite2D
 
-func _on_body_exited(body):
+func _on_body_entered(body):
 	if body.name == "TonyFox":
 		Game.position = self.position
 		print("Checkpoint reached at position:", Game.position)
 		anim.play("Activated")
+		var current_scene = get_tree().current_scene
+		Game.level = current_scene
+		print("Level saved:", Game.level)
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -9,7 +9,7 @@ var inventory = {
 
 var coins = 0
 
-var level = 1
+var level
 
 var position
 
